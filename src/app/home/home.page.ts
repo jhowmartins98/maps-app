0d34b80c2154692638ec142d384c8d47bad51538
jhowmartins98 <jhowhmartins@outlook.com>
2023-10-26 20:23:59 -0300

    att marcador localização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -18,33 +19,62 @@ export class HomePage {
   async exibirMapa(){
      
     // The location of Uluru
-  const position = { lat: -22.511680, lng: -48.565984 };
+  const position = { lat: -22.516133, lng: -48.566900 };
 
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+  
 
   // The map, centered at Uluru
   this.map = new Map(
     this.mapRef.nativeElement,
     {
-      zoom: 20,
+      zoom: 16,
       center: position,
       mapId: 'DEMO_MAP_ID',
     }
   );
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: this.map,
-    position: position,
-    title: 'Uluru'
-  });
+  this.buscarLocalizacao();
+
   }
+ 
 
   ionViewWillEnter(){
     this.exibirMapa();
+  }
+
+  async buscarLocalizacao(){
+    
+    const coordinates = await Geolocation.getCurrentPosition({enableHighAccuracy: true});
+
+  console.log('Current position:', coordinates);
+
+  this.map.setCenter({
+    lat: coordinates.coords.latitude, 
+    lng: coordinates.coords.longitude
+  })
+
+  this.map.setZoom(18);
+
+  this.adicionarMarcador(coordinates);
+  
+  }
+
+  async adicionarMarcador(position: Position){
+
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+    const marker = new AdvancedMarkerElement({
+      map: this.map,
+      position:{
+        lat: position.coords.latitude, 
+        lng: position.coords.longitude
+      },
+      title: 'Marcador'
+    }); 
+    
   }
 
 }
